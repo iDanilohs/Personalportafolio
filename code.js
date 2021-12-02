bd8f3ca3bbf07114fda81163debe9e631f5570f3
Danilo Herrera <daniloherrera1702@gmail.com>
2021-12-01 23:47:58 -0500

    A way for have a form

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 $(".hide").on('click', function() {
     $("nav ul").toggle('slow');
       })
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#truco')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  // console.log(form.get('name'))
+  $buttonMailto.setAttribute('href', `mailto:daniloherrera1702@gmail.com?subject= name: ${form.get('name')} email: ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
